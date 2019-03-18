51395f118f1b5eaa05d67026c34a7808280aa0f2
XAVIER <XAVIER@XAVIER-PC>
2019-03-19 03:04:45 +0530

    new change in login file and cofig reader file

--- src/test/java/test/Login.java
+++ src/test/java/test/Login.java
@@ -2,16 +2,23 @@ package test;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import util.BeforeAfterTest;
+import util.TestUtil;
 
-public class Login extends BeforeAfterTest {
-	WebDriver driver = this.driver;
+public class Login{
 	Logger log = Logger.getLogger(Login.class);
 	@Test
 	public void login1(){
-		
+		WebDriver driver=null;
+		TestUtil ut= new TestUtil();
+		try{
+			driver=ut.chrome();
+		}catch(Exception e){
+			log.error("driver not initilase: " + e.getMessage());
+			Assert.assertNotEquals(null, driver);
+		}
 		
 	}
 }
